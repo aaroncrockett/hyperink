f2d47d48207ef0dce1a00825db976a51b35f7641
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-03-14 22:28:18 -0700

    style: minor page styles passed in

--- customer-sites/aarondoesink/ui/page/index.tsx
+++ customer-sites/aarondoesink/ui/page/index.tsx
@@ -5,7 +5,7 @@ type PageProps = ComponentPropsWithoutRef<"div">;
 
 export default function Page({ children, className, ...props }: PageProps) {
   return (
-    <div className={cn("w-full lg:w-xl", className)} {...props}>
+    <div className={cn("w-full lg:w-xl p-2 pt-3 lg:p-4", className)} {...props}>
       {children}
     </div>
   );
