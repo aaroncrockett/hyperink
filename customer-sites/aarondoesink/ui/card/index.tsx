d421937c18b963200568477bfec809f9874b52dd
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-03-14 21:47:49 -0700

    chore: adjust card component

--- customer-sites/aarondoesink/ui/card/index.tsx
+++ customer-sites/aarondoesink/ui/card/index.tsx
@@ -1,7 +1,14 @@
+import { ComponentPropsWithoutRef } from "react";
 import { cn } from "@/utils/cn";
 
-const cls = "card p-4 space-y-4 ";
+const cls = "card p-4 space-y-4";
 
-export default function Card({ children }: { children: React.ReactNode }) {
-  return <div className={cn(cls)}>{children}</div>;
+type CardProps = ComponentPropsWithoutRef<"div">;
+
+export default function Card({ children, className, ...props }: CardProps) {
+  return (
+    <div className={cn(cls, className)} {...props}>
+      {children}
+    </div>
+  );
 }
