08ade2dfc5f7d9ca63720ca96454a68ac0b58299
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-03-19 10:18:33 -0700

    chore: remove unneeded commented code

--- customer-sites/aarondoesink/app/(footer)/Footer.tsx
+++ customer-sites/aarondoesink/app/(footer)/Footer.tsx
@@ -14,16 +14,6 @@ export default function Footer({ children, className, ...props }: FooterProps) {
       className={cn(styles.footer, "relative bg-primary-500 w-full", className)}
       {...props}
     >
-      {/* <div className="absolute -top-1 lg:-top-2 left-0 w-full pointer-events-none">
-        <Image
-          src="/images/footer-top.png"
-          alt="Footer top decoration"
-          width={4214}
-          height={40}
-          className="w-auto h-auto mx-auto block filter brightness-90 saturate-110 hue-rotate-[3deg]"
-        />
-      </div> */}
-
       {children}
     </footer>
   );
