30bdf7375480b977f5211ece6d09390f9621f42a
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-04-03 18:51:41 -0700

    chore: add supabase to adi

---
+++ customer-sites/aarondoesink/proxy.ts
@@ -0,0 +1,6 @@
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/proxy";
+
+export async function proxy(request: NextRequest) {
+  return await updateSession(request);
+}
