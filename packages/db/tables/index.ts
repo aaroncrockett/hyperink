dd7d5fc96fdc15298b86c2bebbf61266805f35fa
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-04-03 20:31:04 -0700

    adjust db types

--- packages/db/tables/index.ts
+++ packages/db/tables/index.ts
@@ -1,27 +1,37 @@
-export const tattooStyles = [
+import type { UserImage } from "./types";
+
+type TattooStyle = NonNullable<UserImage["styles"]>[number];
+type TattooCollection = NonNullable<UserImage["collection"]>;
+type TattooGroup = NonNullable<UserImage["groups"]>[number];
+type TattooTag = NonNullable<UserImage["tags"]>[number];
+
+export const tattooStyles: TattooStyle[] = [
   "traditional",
   "illustrational",
   "blackwork",
   "micro",
   "photo-realism",
-].map((item) => item.toLowerCase());
+].map((item) => item.toLowerCase() as TattooStyle);
 
-export const tattooCollections = [
+export const tattooCollections: TattooCollection[] = [
   "Queer & Spicey - Filtered",
   "Queery & Spicey - Collage",
   "Neo-expressionist",
   "Pixel Tatts",
   "Gaymer/Anime",
   "Just Whatever",
-].map((item) => item.toLowerCase());
+].map((item) => item.toLowerCase() as TattooCollection);
 
-export const tattooTags = [
+export const tattooTags: TattooTag[] = [
   "color",
   "blackwork",
   "black & gray",
   "photo-based illustrational",
-].map((item) => item.toLowerCase());
+].map((item) => item.toLowerCase() as TattooTag);
 
-export const tattooGroups = ["flash", "tattoos", "portfolio-tattoos", "hp"].map(
-  (item) => item.toLowerCase(),
-);
+export const tattooGroups: TattooGroup[] = [
+  "flash",
+  "tattoos",
+  "portfolio-tattoos",
+  "hp",
+].map((item) => item.toLowerCase() as TattooGroup);
