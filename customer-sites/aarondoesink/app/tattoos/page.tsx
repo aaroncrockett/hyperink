3bf9db8e1e5c37c9f844b781c0548f7affa178d3
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-04-03 20:15:38 -0700

    init tattoos page

--- customer-sites/aarondoesink/app/tattoos/page.tsx
+++ customer-sites/aarondoesink/app/tattoos/page.tsx
@@ -1,3 +1,15 @@
-export default function Tattoos() {
-  return <div>tattoos</div>;
+import { data } from "./index";
+
+export default async function Tattoos() {
+  // if (error) {
+  //   console.error(error);
+  //   return <div>Error loading images</div>;
+  // }
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Tattoos</h1>
+      <p>{data ? data[0].name : null}</p>
+    </div>
+  );
 }
