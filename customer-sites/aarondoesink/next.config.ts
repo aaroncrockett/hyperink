b6d44cc723eb636dc72456ae078db6384cc43af1
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-04-05 19:15:23 -0700

    chore: update config for images urls

--- customer-sites/aarondoesink/next.config.ts
+++ customer-sites/aarondoesink/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+https://scthxmchtmcisjstzjpe.supabase.co/storage/v1/object/public/user-images/e8222b31-fc36-46c5-a8f9-6287c268d1ce/51e6121d-bf71-4431-9942-dd5d0b1128c7
+
+https://scthxmchtmcisjstzjpe.supabase.co/storage/v1/object/sign/user-images/e8222b31-fc36-46c5-a8f9-6287c268d1ce/51e6121d-bf71-4431-9942-dd5d0b1128c7?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV84ZjM4YmJkYS04Y2NkLTQzNmMtOWFmNC0wZWI5MWVmZjMxYjIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJ1c2VyLWltYWdlcy9lODIyMmIzMS1mYzM2LTQ2YzUtYThmOS02Mjg3YzI2OGQxY2UvNTFlNjEyMWQtYmY3MS00NDMxLTk5NDItZGQ1ZDBiMTEyOGM3IiwiaWF0IjoxNzc1Mjc1MTIzLCJleHAiOjE4MDY4MTExMjN9.wwjMobFxATyRexVp5PwVAbG_KqYFKcZU8-qdiaFnVIc
