f1b38995656e74c52722175de6541b32fb4f0a8c
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-07-21 19:39:32 -0700

    wip

--- apps/cms/app/admin/client/page.tsx
+++ apps/cms/app/admin/client/page.tsx
@@ -18,7 +18,7 @@ import { getLastTenClients, ALL_CLIENT_COLS_KEY_VAL } from "@/utils/db";
 // Local
 import { LINKS_ADMIN } from "@/app/consts";
 
-const gridCls = "grid grid-cols-[12rem_10rem_10rem_20rem_12rem_12rem_12rem]";
+const gridCls = "grid grid-cols-[6rem_6rem_10rem_10rem_10rem_8rem_8rem]";
 
 export default async function ClientPage() {
   const authedClient = await createServerClientAndAuth();
@@ -27,20 +27,24 @@ export default async function ClientPage() {
   return (
     <Page className="">
       <Heading as="h1" text="Client Records" />
-
-      <Link className="bg-primary-500" href={LINKS_ADMIN.createClient.href}>
-        {LINKS_ADMIN.createClient.label}
-      </Link>
+      <div className="flex gap-3">
+        <Link
+          className="text-primary-500  underline p-2 pt-3"
+          href={LINKS_ADMIN.createClient.href}
+        >
+          {LINKS_ADMIN.createClient.label}
+        </Link>
+      </div>
 
       <TableLayout
         gridCls={gridCls}
         headerCols={
           <>
+            <span>Update Client</span>
+            <span>Create Tatt Record</span>
             <span>{ALL_CLIENT_COLS_KEY_VAL.preferred_name.label}</span>
             <span>{ALL_CLIENT_COLS_KEY_VAL.first_name.label}</span>
             <span>{ALL_CLIENT_COLS_KEY_VAL.last_name.label}</span>
-            <span>{ALL_CLIENT_COLS_KEY_VAL.email.label}</span>
-            <span>{ALL_CLIENT_COLS_KEY_VAL.phone.label}</span>
             <span>{ALL_CLIENT_COLS_KEY_VAL.created_at.label}</span>
             <span>{ALL_CLIENT_COLS_KEY_VAL.updated_at.label}</span>
           </>
@@ -54,6 +58,8 @@ export default async function ClientPage() {
             )}
             key={client.id}
           >
+            <span className="bg-primary-400-600 truncate">update </span>
+            <span className="bg-primary-400-600 truncate">create</span>
             <span className="truncate font-semibold">
               {client[ALL_CLIENT_COLS_KEY_VAL.preferred_name.value]}
             </span>
@@ -64,14 +70,6 @@ export default async function ClientPage() {
 
             <span className="truncate">
               {client[ALL_CLIENT_COLS_KEY_VAL.last_name.value]}
-            </span>
-
-            <span className="truncate">
-              {client[ALL_CLIENT_COLS_KEY_VAL.email.value]}
-            </span>
-
-            <span className="truncate">
-              {client[ALL_CLIENT_COLS_KEY_VAL.phone.value]}
             </span>
 
             <span className="truncate">
