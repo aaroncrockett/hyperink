115d4d8b2289488ce500095aef6bbeab81428492
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-07-22 12:26:39 -0700

    wip

--- apps/cms/app/admin/_components/Nav.tsx
+++ apps/cms/app/admin/_components/Nav.tsx
@@ -1,19 +1,29 @@
 "use client";
 
+import { Home, TagIcon, Zap, PenTool, User, Image } from "lucide-react";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@hyperinkstudio/utils/cn";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+import { LucideProps } from "lucide-react";
 
 type NavsProps = {
   links: {
     href: string;
     label: string;
+    icon: string;
+    showIcon: boolean;
   }[];
-  layout?: "col" | "row";
   cls?: string;
   gap?: string;
+  iconSizeCls?: string;
+  iconCls?: string;
   textSize?: string;
+  layout?: "col" | "row";
   linkCls?: string;
+  linkClsGap?: string;
+  linkClsLayout?: string;
   linkClsColor?: string;
   linkClsPadding?: string;
   linkClsHover?: string;
@@ -21,36 +31,59 @@ type NavsProps = {
 };
 
 export function Nav({
-  links,
-  layout = "row",
   cls = "",
   gap = "gap-2",
-  textSize = "",
-  linkCls = "",
+  iconSizeCls = "w-5 h-5",
+  iconCls = "",
+  layout = "row",
+  linkCls = "flex flex-row",
   linkClsColor = "text-primary-400-600",
-  linkClsPadding = "px-3 py-2",
+  linkClsGap = "gap-2",
   linkClsHover = " hover:underline",
+  linkClsLayout = "flex flex-row items-center",
+  linkClsPadding = "px-3 py-2",
   linkCurrentCls = "text-surface-400-600! underline",
+  links,
+  textSize = "",
 }: NavsProps) {
   const pathname = usePathname();
 
   const flexLayout = layout === "row" ? "flex flex-row" : "flex flex-col";
 
+  const ICONS: Record<
+    string,
+    ForwardRefExoticComponent<
+      Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+    >
+  > = {
+    Home: Home,
+    TagIcon: TagIcon,
+    Zap: Zap,
+    PenTool: PenTool,
+    User: User,
+    Image: Image,
+  };
+
   return (
     <ul className={cn(flexLayout, gap, textSize, cls)}>
       {links.map((link) => {
+        const Icon = link.icon && link.showIcon ? ICONS[link.icon] : null;
+
         return (
           <li key={link.href}>
             <Link
               className={cn(
                 linkCls,
                 linkClsColor,
-                linkClsPadding,
+                linkClsGap,
                 linkClsHover,
+                linkClsLayout,
+                linkClsPadding,
                 pathname === link.href && linkCurrentCls,
               )}
               href={link.href}
             >
+              {Icon && <Icon className={cn(iconCls, iconSizeCls)} />}
               {link.label}
             </Link>
           </li>
--- apps/cms/app/consts.ts
+++ apps/cms/app/consts.ts
@@ -2,30 +2,44 @@ export const LINKS_ADMIN = {
   home: {
     href: "/admin",
     label: "Admin Home",
+    icon: "Home",
+    showIcon: true,
   },
   client: {
     href: "/admin/client",
     label: "Client Records",
+    icon: "User",
+    showIcon: true,
   },
   createClient: {
     href: "/admin/client/create-client",
     label: "Create a Client",
+    icon: "User",
+    showIcon: true,
   },
   createTattooRecord: {
     href: "/admin/create-tattoo-record",
     label: "Create a tattoo record",
+    icon: "PenTool",
+    showIcon: true,
   },
   uploadTattooImages: {
     href: "/admin/upload-tattoo-images",
     label: "Upload tattoo images",
+    icon: "Image",
+    showIcon: true,
   },
   createFlash: {
     href: "/admin/create-flash",
     label: "Create Flash",
+    icon: "Zap",
+    showIcon: true,
   },
   createProfileTaggingOptions: {
     href: "/admin/create-tagging-opts",
     label: "Tagging Options",
+    icon: "TagIcon",
+    showIcon: true,
   },
 } as const;
 
@@ -40,10 +54,13 @@ export const NAV_ADMIN = {
 export const NAV_ADMIN_CLIENT = {
   client: {
     ...LINKS_ADMIN.client,
+    showIcon: false,
   },
   createClient: {
     href: "/admin/client/create-client",
     label: "Create a Client",
+    icon: "User",
+    showIcon: false,
   },
 };
 export const LINKS_ADMIN_LIST = Object.values(LINKS_ADMIN);
