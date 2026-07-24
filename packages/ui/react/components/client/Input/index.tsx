aedb262d41f98ab6ccc91a9d63658084b62a0681
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-07-23 21:05:55 -0700

    wip

--- packages/ui/react/components/client/Input/index.tsx
+++ packages/ui/react/components/client/Input/index.tsx
@@ -1,38 +1,46 @@
 import { cn } from "@hyperinkstudio/utils/";
 
 type InputProps = {
-  id: string;
-  name: string;
-  label: string;
-  type?: React.HTMLInputTypeAttribute;
-  errors?: Record<string, string> | null;
   className?: string;
-  labelClassName?: string;
   errorCls?: string;
+  errors?: Record<string, string> | null;
   errorTxtColor?: string;
+  id: string;
+  label: string;
+  labelClassName?: string;
+  name: string;
+  required?: boolean;
+  type?: React.HTMLInputTypeAttribute;
+  value?: string;
 };
 
 export function Input({
-  id,
-  name,
-  label,
-  type = "text",
-  errors = {},
   className = "",
-  labelClassName = "",
   errorCls = "",
+  errors = {},
   errorTxtColor = "text-red-500",
+  id,
+  label,
+  labelClassName = "",
+  name,
+  required = false,
+  type = "text",
+  value,
 }: InputProps) {
   return (
     <div>
-      <label htmlFor={id} className={cn("label", labelClassName)}>
-        {label}
-      </label>
+      {type !== "hidden" && (
+        <label htmlFor={id} className={cn("label", labelClassName)}>
+          {label}
+        </label>
+      )}
 
       <input
         id={id}
         name={name}
         type={type}
+        required={required}
+        {...(value !== undefined ? { value } : {})}
         className={cn("input", className)}
       />
 
