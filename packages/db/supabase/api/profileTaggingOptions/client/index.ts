7d813344b4994b074e9cae79488e9cb8534dbb9f
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-08-08 18:47:46 -0700

    refactor: remove unused and commented out file

--- packages/db/supabase/api/profileTaggingOptions/client/index.ts
+++
@@ -1,8 +0,0 @@
-// import type { SupabaseClient } from "@hyperinkstudio/db";
-// import { TABLE_PROFILE_TAGGING_OPTS as TABLE } from "../consts";
-
-// export async function getProfileTaggingOpts(browserClient: SupabaseClient) {
-//   const { data, error } = await browserClient.from(TABLE).select("*");
-
-//   return { data, error };
-// }
