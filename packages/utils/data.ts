61569372fcf7e543950d954b36d498f0acfacb2b
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-08-18 15:42:03 -0700

    feat: toLabelValue function

--- packages/utils/data.ts
+++ packages/utils/data.ts
@@ -9,3 +9,12 @@ export const getKeysFromCollection = <T extends Record<PropertyKey, unknown>>(
 export const getValuesFromCollection = <T extends Record<PropertyKey, unknown>>(
   collection: T,
 ): T[keyof T][] => Object.values(collection) as T[keyof T][];
+
+export function toLabelValue(value: string) {
+  return {
+    value,
+    label: value
+      .replace(/[_,-]|\+|-/g, " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase()),
+  };
+}
