deeaf6699fb63e32f48f32911b62ca2b32d4c534
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-02 08:45:31 -0700

    refine: abstract a get function in services

--- packages/services/supabase/get/index.ts
+++ packages/services/supabase/get/index.ts
@@ -22,24 +22,76 @@ type Options = {
   [key: string]: unknown;
 };
 
-export async function get<T extends AllowedTable>(
-  client: Client,
-  table: T,
-  values: (keyof AppInserts[T])[],
+const handleOrder = <T extends { order: Function }>(
+  query: T,
   opts: Options,
-) {
-  const selectFields = values.length ? values.join(", ") : "*";
-
-  let query = client.from(table as any).select(selectFields);
-
+) => {
   if (opts?.order) {
     query = query.order(opts.order.name, opts.order.opts as any);
   } else {
     query = query.order("updated_at", { ascending: false });
   }
 
+  return query;
+};
+
+const handleLimit = <T extends { limit: Function }>(
+  query: T,
+  opts: Options,
+) => {
   if (opts?.limit) {
     query = query.limit(opts.limit);
+  }
+
+  return query;
+};
+
+export async function get<T extends AllowedTable>(
+  client: Client,
+  table: T,
+  values: Partial<AppInserts[T]>[],
+  opts?: Options,
+) {
+  const selectFields = values.length ? values.join(", ") : "*";
+
+  let query = client.from(table as any).select(selectFields);
+
+  if (opts) {
+    query = handleOrder(query, opts);
+    query = handleLimit(query, opts);
+  }
+
+  const { data, error } = await query;
+
+  return {
+    data: data as AppTables[T][] | null,
+    error,
+  };
+}
+
+type By<T extends AllowedTable> = {
+  field: keyof AppTables[T];
+  value: AppTables[T][keyof AppTables[T]];
+};
+
+export async function getBy<T extends AllowedTable>(
+  client: Client,
+  table: T,
+  values: Partial<AppInserts[T]>[],
+  by: By<T>[],
+  opts?: Options,
+) {
+  const selectFields = values.length ? values.join(", ") : "*";
+
+  let query = client.from(table as any).select(selectFields);
+
+  for (const condition of by) {
+    query = query.eq(condition.field as string, condition.value);
+  }
+
+  if (opts) {
+    query = handleOrder(query, opts);
+    query = handleLimit(query, opts);
   }
 
   const { data, error } = await query;
