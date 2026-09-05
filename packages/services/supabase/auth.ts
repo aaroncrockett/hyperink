06d52b765374ee2dfa053182cad17a6a62c9dcfb
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-04 19:21:05 -0700

    refine: var name adjustment

--- packages/services/supabase/auth.ts
+++ packages/services/supabase/auth.ts
@@ -7,31 +7,28 @@ import type {
   SignInWithOAuth,
 } from "./server/types";
 
-export const verifyOtp: VerifyOtp = async (authedClient, data) => {
-  return authedClient.auth.verifyOtp(data);
+export const verifyOtp: VerifyOtp = async (client, data) => {
+  return client.auth.verifyOtp(data);
 };
 
-export const signInWithPassword: SignInWithPassword = async (
-  authedClient,
-  data,
-) => {
-  return authedClient.auth.signInWithPassword(data);
+export const signInWithPassword: SignInWithPassword = async (client, data) => {
+  return client.auth.signInWithPassword(data);
 };
 
-export const signUp: SignUp = async (authedClient, data) => {
-  return authedClient.auth.signUp(data);
+export const signUp: SignUp = async (client, data) => {
+  return client.auth.signUp(data);
 };
-export const signInWithOAuth: SignInWithOAuth = async (authedClient, data) => {
-  return authedClient.auth.signInWithOAuth(data);
+export const signInWithOAuth: SignInWithOAuth = async (client, data) => {
+  return client.auth.signInWithOAuth(data);
 };
 
 export const exchangeCodeForSession: ExchangeCodeForSession = async (
-  authedClient,
+  client,
   code,
 ) => {
-  return authedClient.auth.exchangeCodeForSession(code);
+  return client.auth.exchangeCodeForSession(code);
 };
 
-export const getUser: GetUser = async (authedClient) => {
-  return authedClient.auth.getUser();
+export const getUser: GetUser = async (client) => {
+  return client.auth.getUser();
 };
