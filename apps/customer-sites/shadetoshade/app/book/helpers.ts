5c783aa5a27cd9546ddbe959e6c1c2080687e99e
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-04 17:44:41 -0700

    Feat(#64) setup email clients, post booking

---
+++ apps/customer-sites/shadetoshade/app/book/helpers.ts
@@ -0,0 +1,36 @@
+export function customerEmail() {
+  return `
+<h1>You booked a tattoo with me :D</h1>
+<p>
+            When you are ready, a booking fee or deposit is required to move forward.
+            for more info see <a href="https://shadetoshadetattoo.com/book/thank-you">booking thanks page</a>
+</p>
+</div>
+
+  
+<p>....</p>
+<p>....</p>
+        <ul style="background:#ffe6ec; border:1px solid #f4b5c1; padding:10px 14px; border-radius:6px; display:inline-block; margin:10px 0;" >
+          <li>
+            <strong>Venmo
+        
+            : @Aaron-Happe</strong>
+            <li>
+              <strong>https://venmo.com/u/Aaron-Happe </strong>
+            </li>
+          </li>
+          <li>
+            <strong>Cash App: $aaromichaelhappe</strong>
+            <li>
+              <strong> $aaromichaelhappe</strong>
+            </li>
+          </li>
+        </ul>
+        <p>....</p>
+<p>....</p>
+<p>Please see <a href="https://shadetoshadetattoo.com/faq">My FAQ</a> if you have any questions.</p>
+<p>
+  Feel free to email me at shade.to.shade.artx@gmail.com if you have anymore questions.
+</p>
+<p>Talk to you soon! - Aaron (ShadeToShadeTattoo)</p>`;
+}
