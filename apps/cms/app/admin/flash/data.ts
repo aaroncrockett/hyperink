b8756f860327564ffb1904c4510b316605040bf4
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-17 07:47:21 -0700

    feat: init api types and flash types

---
+++ apps/cms/app/admin/flash/data.ts
@@ -0,0 +1,19 @@
+// import type { UIRowMeta, FlashUIRow } from "@hyperink/api";
+
+// type FlashUI = Pick<UIRowMeta<FlashUIRow>, "id" | "styles">;
+
+// const UIData: FlashUI = {
+//   id: {
+//     id: "id",
+//     label: "ID",
+//     readOnly: true,
+//     display: false,
+//   },
+
+//   styles: {
+//     id: "styles",
+//     label: "Styles",
+//     readOnly: false,
+//     display: true,
+//   },
+// };
---
+++ packages/api/types/index.ts
@@ -0,0 +1,28 @@
+import { MergeDeep } from "type-fest";
+import { Database as DatabaseGenerated } from "@hyperink/service-providers";
+import { CustomFlashRow as CustomFlashRowSrc } from "./flash";
+export type { FlashTagging, FlashUIRow } from "./flash";
+
+export type Database = MergeDeep<
+  DatabaseGenerated,
+  {
+    public: {
+      Tables: {
+        flash: {
+          Row: CustomFlashRowSrc;
+        };
+      };
+    };
+  }
+>;
+
+export type UIMetaData<T, K extends keyof T> = {
+  id: K;
+  label: string;
+  readOnly?: boolean;
+  display?: boolean;
+};
+
+export type UIRowMeta<T> = {
+  [K in keyof T]: UIMetaData<T, K>;
+};
