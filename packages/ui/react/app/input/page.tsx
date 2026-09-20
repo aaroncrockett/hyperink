2429d09fd19eab27eb79e0af393e827a767dad2d
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-19 21:24:29 -0700

    feat: create DocItems comp and use on Input

--- packages/ui/react/app/input/page.tsx
+++ packages/ui/react/app/input/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Input } from "@hyperink/ui-react/components";
-import { div } from "motion/react-client";
+import { DocItems } from "../_components/DocItems";
+import { docs } from "./data";
 import { useState } from "react";
 
 export default function InputPage() {
@@ -12,52 +13,7 @@ export default function InputPage() {
       <div className="gap-3 grid grid-cols-2 docs">
         <div className="bg-secondary-100-900/40 p-4 rounded text-surface-950-50">
           <p className="font-bold">DOCS</p>
-          <ul className="flex flex-col gap-1">
-            <li>desc?: A description</li>
-            <li>descClassName?: adds to description classes</li>
-            <li>descUtilClassName?: overrides description</li>
-            <li>defaultValue?: a default value</li>
-            <li>
-              dir?: &quot; row &quot; OR &quot; col &quot;: Deterlines if flex
-              col or row is used. Also affects wrapper gap and alignments
-            </li>
-            <li>disabled?: boolean;</li>
-            <li>errorClassName?: adds to error class anem</li>
-            <li>errorUtilClassName?: overrides error classes</li>
-            <li>error?: string error message</li>
-            <li>id?: string;</li>
-            <li>
-              inputUtilClassName?: overrides input classes (className is on
-              props, and adds to the input classes);
-            </li>
-            <li>label?: string;</li>
-            <li>labelClassName?: adds to lavel classes</li>
-            <li>labelUtilClassName?: overrides label classes</li>
-            <li>name?: string;</li>
-            <li>placeholder?: string;</li>
-            <li>readOnly?: boolean;</li>
-            <li>required?: boolean;</li>
-            <li>
-              textColorUtilClassName?: overrides text color for all text colors
-              except placeholder and descrtion. If you want to override a
-              specific text color, use !important and pass in as a className
-            </li>
-            <li>type?: React.HTMLInputTypeAttribute;</li>
-            <li>value?: string;</li>
-            <li>
-              wrapperAlignUtilClassName?: overrides alignment classes. This is
-              needed in isolation because the alignment is computed based on the
-              layout prop
-            </li>
-            <li>
-              wrapperGapUtilClassName?: overrides gap classes. Gap is needed in
-              isolation because the gap is computed based on the layout prop
-            </li>
-            <li>wrapperClassName?: adds to wrapper classes</li>
-            <li>wrapperUtilClassName?: overrides wrapper classes</li>
-            <li>...props are applied to the Input element</li>
-          </ul>
-          <div></div>
+          <DocItems items={docs} />
         </div>
         <div className="flex flex-col gap-3">
           <p className="text-surface-950-50">Examples</p>
