618455d973b0e10e8017d60bd3ca4f4e1e017f16
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-21 22:27:31 -0700

    feat: heading component

--- packages/ui/react/components/index.ts
+++ packages/ui/react/components/index.ts
@@ -2,6 +2,7 @@ export { AnimatedMenuIcon } from "./AnimatedMenuIcon";
 export { Button } from "./Button";
 export { ErrorDisplay, ErrorsDisplay } from "./ErrorDisplays";
 export { FormClient } from "./FormClient";
+export { Heading } from "./Heading";
 export { Icon } from "./Icon";
 export { Input } from "./Input";
 export { InputCheck } from "./InputCheck";
