1f5af10af69816f5b277a11356723f6308b41d06
Aaron Crockett <aaron.crockett.00@gmail.com>
2026-09-25 16:13:27 -0700

    bug: add back toLabelValue

--- packages/api-domain-helpers/index.ts
+++ packages/api-domain-helpers/index.ts
@@ -1,2 +1,11 @@
 export * from "./zod";
 export * from "./types";
+
+export function toLabelValue(value: string) {
+  return {
+    value,
+    label: value
+      .replace(/[_,-]|\+|-/g, " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase()),
+  };
+}
